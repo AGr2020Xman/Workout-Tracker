af7e0689d24bbfc5a498ac06437d1e46edc6cd71
Andre Grech <silver.grech@gmail.com>
2020-12-15 17:10:41 +1100

    workout model

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  exercises: [
+    {
+      type: {
+        type: String,
+        enum: ["resistance", "cardio"]
+      },
+      name: {
+        type: String
+      },
+      duration: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      },
+    }
+  ],
+  totalDuration: {
+    type: Number
+  }
+});
+
+const Workout = mongoose.model("Workout", UserSchema);
+
+module.exports = Workout;
