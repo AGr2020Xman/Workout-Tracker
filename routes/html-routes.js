eb292e937e959a2528688fa577b99b2c2ff20cf5
Andre Grech <silver.grech@gmail.com>
2020-12-15 17:12:22 +1100

    establish HTML routing

---
+++ routes/html-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+  
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../public/stats.html"));
+});
+  
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../public/exercise.html"));
+});
+
+module.exports = router;
